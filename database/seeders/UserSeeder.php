e9437a8f1f5f9669ab8ae04d878ee1a5e6fec23c
islamabdelkarim <islamabdelkarimpro@gmail.com>
2025-12-29 18:42:20 +0200

    feat: Add admin user seeding and remove the Notifications Filament page.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\ChatRoom;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -16,9 +17,9 @@ class UserSeeder extends Seeder
     {
         User::create([
             'id' => 1,
-            'first_name' => "admin",
-            'last_name' => "dash",
-            'email' => "admin@gmail.com",
+            'first_name' => 'admin',
+            'last_name' => 'dash',
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -28,9 +29,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 2,
-            'first_name' => "author",
-            'last_name' => "dash",
-            'email' => "author@gmail.com",
+            'first_name' => 'author',
+            'last_name' => 'dash',
+            'email' => 'author@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -40,9 +41,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 3,
-            'first_name' => "ahmed",
-            'last_name' => "khaled",
-            'email' => "ahmed@gmail.com",
+            'first_name' => 'ahmed',
+            'last_name' => 'khaled',
+            'email' => 'ahmed@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -52,9 +53,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 4,
-            'first_name' => "abdo",
-            'last_name' => "khaled",
-            'email' => "abdo@gmail.com",
+            'first_name' => 'abdo',
+            'last_name' => 'khaled',
+            'email' => 'abdo@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -64,9 +65,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 5,
-            'first_name' => "ziad",
-            'last_name' => "el sayed",
-            'email' => "ziad@gmail.com",
+            'first_name' => 'ziad',
+            'last_name' => 'el sayed',
+            'email' => 'ziad@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'pending',
@@ -76,9 +77,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 6,
-            'first_name' => "mohamed",
-            'last_name' => "khaled",
-            'email' => "mohamed@gmail.com",
+            'first_name' => 'mohamed',
+            'last_name' => 'khaled',
+            'email' => 'mohamed@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -88,9 +89,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 7,
-            'first_name' => "ali",
-            'last_name' => "khaled",
-            'email' => "ali@gmail.com",
+            'first_name' => 'ali',
+            'last_name' => 'khaled',
+            'email' => 'ali@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -100,9 +101,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 8,
-            'first_name' => "amr",
-            'last_name' => "khaled",
-            'email' => "amr@gmail.com",
+            'first_name' => 'amr',
+            'last_name' => 'khaled',
+            'email' => 'amr@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -112,9 +113,9 @@ class UserSeeder extends Seeder
 
         User::create([
             'id' => 9,
-            'first_name' => "azab",
-            'last_name' => "azab",
-            'email' => "azab@gmail.com",
+            'first_name' => 'azab',
+            'last_name' => 'azab',
+            'email' => 'azab@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'status' => 'active',
@@ -122,9 +123,16 @@ class UserSeeder extends Seeder
             'role_id' => 1,
         ]);
         ChatRoom::create([
-            "id" => 20,
-            "chat_type_id" => 2,
-            "sender_id" => 1,
+            'id' => 20,
+            'chat_type_id' => 2,
+            'sender_id' => 1,
+        ]);
+        Admin::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' =>Hash::make('123456789'),
+            'phone_number' => '01232333333',
+            'role_id'=>1,
         ]);
 
     }
